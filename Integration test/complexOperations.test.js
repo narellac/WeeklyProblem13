24ff569f82b8f82d4876968b70ed8fa39ec0481a
Narella Calvente <narellacalvente@gmail.com>
2021-05-01 15:40:16 -0300

    BQA-13: creation of code

--- Integration test/complexOperations.test.js
+++ Integration test/complexOperations.test.js
@@ -3,7 +3,7 @@ import * as complexOperations from './complexOperations';
 
 describe('complexOperation - Unit Tests', () => {
   describe('checkEmail', () => {
-    it('first test for checkEmail', () => {
+    it('First test for checkEmail', () => {
       expect(complexOperations.checkEmail(10)).toBe('The email should be an string');
     });
     it('Second test for checkEmail', () => {
@@ -15,31 +15,78 @@ describe('complexOperation - Unit Tests', () => {
   });
 
   describe('calculateArea', () => {
-    it('first test for calculateArea', () => {
-
+    it('First test for calculateArea', () => {
+      expect(complexOperations.calculateArea('hexagon')).toBe(`${'hexagon'} is not supported`);
+    });
+    it('Second test for calculateArea', () => {
+      expect(complexOperations.calculateArea('square', 'five', 'ten')).toBe('number1 and number2 should be numbers');
+    });
+    it('Third test for calculateArea', () => {
+      expect(complexOperations.calculateArea('square', 5, 5)).toBe(25);
+    });
+    it('Four test for calculateArea', () => {
+      expect(complexOperations.calculateArea('rectangle', 5, 10)).toBe(50);
+    });
+    it('Five test for calculateArea', () => {
+      expect(complexOperations.calculateArea('triangle', 4, 8)).toBe(16);
+    });
+    it('Six test for calculateArea', () => {
+      expect(complexOperations.calculateArea('circle', 10)).toBeCloseTo(314.16);
     });
   });
 
   describe('sumGratherThan', () => {
-    it('first test for sumGratherThan', () => {
-
+    it('First test for sumGratherThan', () => {
+      expect(complexOperations.sumGratherThan('five', 'six', 'seven')).toBe('The params should be numbers');
+    });
+    it('Second test for sumGratherThan', () => {
+      expect(complexOperations.sumGratherThan(4, 5, 6)).toBe(`${9} is grather than ${6}`);
+    });
+    it('Third test for sumGratherThan', () => {
+      expect(complexOperations.sumGratherThan(2, 2, 6)).toBe(`${4} is less than ${6}`);
     });
   });
 
   describe('intersectionBetweenArrays', () => {
-    it('first test for intersectionBetweenArrays', () => {
-
+    it('First test for intersectionBetweenArrays', () => {
+      expect(complexOperations.intersectionBetweenArrays('hello', 5)).toBe('The params should be arrays');
+    });
+    it('Second test for intersectionBetweenArrays', () => {
+      expect(complexOperations.intersectionBetweenArrays(['apple', 'banana', 'orange'], ['one', 'two', 'three'])).toBe('There are not matching elements');
+    });
+    it('Third test for intersectionBetweenArrays', () => {
+      expect(complexOperations.intersectionBetweenArrays(['apple', 'banana', 'orange'], ['one', 'apple', 'three'])).toStrictEqual(['apple']);
     });
   });
 
   describe('sortArrayOfObjectsByKey', () => {
-    it('first test for sortArrayOfObjectsByKey', () => {
-
+    it('First test for sortArrayOfObjectsByKey', () => {
+      expect(complexOperations.sortArrayOfObjectsByKey('hello')).toBe('The first param should be an array');
     });
-  });
+    it('Second test for sortArrayOfObjectsByKey', () => {
+      expect(complexOperations.sortArrayOfObjectsByKey([5, 6])).toBe('The second param should be an string');
+    });
+    /*it('Third test for sortArrayOfObjectsByKey', () => {
+      expect(complexOperations.sortArrayOfObjectsByKey(['Narella', 26])).toBe(`Some elements in the array does not have the ${' '} property`);
+    });
+    it('Four test for sortArrayOfObjectsByKey', () => {
+      expect(complexOperations.sortArrayOfObjectsByKeys({firstName:'Narella', lastName:'Calvente', age:'25'})).toStrictEqual({age:'25', firstName:'Narella', lastName:'Calvente'});
+    });
+  });*/
 
   describe('numberOfOddAndEvenNumbers', () => {
-    it('first test for numberOfOddAndEvenNumbers', () => {
+    it('First test for numberOfOddAndEvenNumbers', () => {
+      expect(complexOperations.numberOfOddAndEvenNumbers('hello')).toBe('The param should be an array');
+    });
+    it('Second test for numberOfOddAndEvenNumbers', () => {
+      expect(complexOperations.numberOfOddAndEvenNumbers(['hello', 'world'])).toBe(`The array should have only numbers`);
+    });
+    it('Third test for numberOfOddAndEvenNumbers', () => {
+      expect(complexOperations.numberOfOddAndEvenNumbers([4, 6, 8])).toStrictEqual({"even": 3, "odd": 0});
+    });
+    it('Four test for numberOfOddAndEvenNumbers', () => {
+      expect(complexOperations.numberOfOddAndEvenNumbers([2, 4, 6, 8, 10])).toStrictEqual({"even": 5, "odd": 0});
     });
   });
 });
+})
